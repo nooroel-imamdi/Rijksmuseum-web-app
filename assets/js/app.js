f603bc3119ebae4157ea72d0522f72f21279e41f
Nooroel Imamdi <nooroelimamdi@gmail.com>
2017-05-13 18:02:52 +0200

    detailpage works 🎉

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,19 +5,21 @@
     config: {
       BASE_URL: apibaseurl,
       SEARCH_QUERY: '?q=',
-      API_KEY_QUERY: '&key=' + apikey + '&format=json'
+      API_KEY_QUERY: 'key=' + apikey + '&format=json'
     },
     cache: {
       paintings: [],
       paint: []
     },
     htmlElements: {
-      userInputField: document.querySelector("#user-input-field"),
-      searchForm: document.querySelector("#search-form"),
-      searchFeedback: document.querySelector("#search-feedback"),
-      randomPaintingTemplate: document.querySelector("#random-paintings-template"),
-      randomPaintingOuput: document.querySelector("#random-paintings-output"),
-      errorPage: document.querySelector("#error"),
+      userInputField: document.querySelector('#user-input-field'),
+      searchForm: document.querySelector('#search-form'),
+      searchFeedback: document.querySelector('#search-feedback'),
+      randomPaintingTemplate: document.querySelector('#random-paintings-template'),
+      randomPaintingOuput: document.querySelector('#random-paintings-output'),
+      detailPaintingTemplate: document.querySelector('#detail-painting-template'),
+      detailPaintingOuput: document.querySelector('#detail-painting-output'),
+      errorPage: document.querySelector('#error'),
       loader: document.querySelector('.loader')
     },
     init: function() {
@@ -30,10 +32,10 @@
     init: function() {
       routie({
         '': function() {
-          collection.get();
+          collection.getRandom();
         },
-        ':principalMakers': function(principalMakers) {
-
+        'paint/:objectNumber': function(objectNumber) {
+          collection.getDetail(objectNumber);
         }
       });
     }
@@ -46,29 +48,32 @@
       } else {
         app.htmlElements.loader.classList.add('hide');
       }
+    },
+    renderDetail(){
+      app.htmlElements.randomPaintingOuput.classList.add('hide');
     }
   };
 
   var collection = {
-    search: function() {
-      // app.htmlElements.searchForm.addEventListener("submit", searchQuery)
-
-      app.htmlElements.searchForm.addEventListener("submit", function() {
-        var userInput = app.htmlElements.userInputField.value;
-        console.log(userInput);
-        collection.get(userInput);
-
-        if (query.length > 0) {
-
-        }
-
-      });
-    },
-    get: function() {
+    // search: function() {
+    //   // app.htmlElements.searchForm.addEventListener('submit', searchQuery)
+    //
+    //   app.htmlElements.searchForm.addEventListener('submit', function() {
+    //     var userInput = app.htmlElements.userInputField.value;
+    //     console.log(userInput);
+    //     collection.getRandom(userInput);
+    //
+    //     if (query.length > 0) {
+    //
+    //     }
+    //
+    //   });
+    // },
+    getRandom: function() {
       sections.loaderState('show');
       var request = new window.XMLHttpRequest();
-      var url = app.config.BASE_URL + app.config.SEARCH_QUERY + '' + app.config.API_KEY_QUERY;
-			request.open("GET", url, true);
+      var url = app.config.BASE_URL + app.config.SEARCH_QUERY + '' + '&' + app.config.API_KEY_QUERY;
+			request.open('GET', url, true);
 			request.onload = function() {
         if (request.status >= 200 && request.status < 400) {
           // Success!
@@ -89,7 +94,49 @@
 
           // app.cache.paintings.push(collectionObject);
           } else {
-          // We reached our target server, but it returned an error
+          // We reached our targetRandom server, but it returned an error
+          sections.loaderState('hide');
+        }
+
+			};
+
+			request.onerror = function() {
+			   // There was a connection error of some sort
+         sections.loaderState('hide');
+         // Show error page
+         app.htmlElements.errorPage.classList.remove('hide');
+			};
+
+			request.send();
+    },
+    getDetail: function(objectNumber) {
+      sections.loaderState('show');
+      sections.renderDetail();
+      var request = new window.XMLHttpRequest();
+      var url = app.config.BASE_URL + '/' + objectNumber + '?&' + app.config.API_KEY_QUERY;
+			request.open('GET', url, true);
+			request.onload = function() {
+        if (request.status >= 200 && request.status < 400) {
+          // Success!
+          var data = JSON.parse(request.responseText);
+          renderHandlebars.detailPainting(data);
+
+          console.log(data);
+          setTimeout(function() {
+            sections.loaderState('hide');
+          }, 1000);
+          // var collectionObject = Object.keys(data).map(function (key) {
+          //   console.log(data);
+          //
+          //   return {
+          //     title: data.artObjects[key].title,
+          //   };
+          //
+          // });
+
+          // app.cache.paintings.push(collectionObject);
+          } else {
+          // We reached our targetRandom server, but it returned an error
           sections.loaderState('hide');
         }
 
@@ -113,7 +160,14 @@
 			var ourGeneratedHTML = compiledTemplate(all);
       var outputData = app.htmlElements.randomPaintingOuput;
       outputData.innerHTML = ourGeneratedHTML;
-    }
+    },
+    detailPainting: function(detail) {
+      var rawTemplating = app.htmlElements.detailPaintingTemplate.innerHTML;
+			var compiledTemplate = Handlebars.compile(rawTemplating);
+			var ourGeneratedHTML = compiledTemplate(detail);
+      var outputData = app.htmlElements.detailPaintingOuput;
+      outputData.innerHTML = ourGeneratedHTML;
+    },
   };
   app.init();
 
